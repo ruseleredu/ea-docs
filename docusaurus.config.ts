66db6f955240efcad3198030db45668322669301
Adriano Ruseler <adrianoruseler@gmail.com>
2026-01-26 14:28:26 -0300

    Update site config for ELT82E and add resources
    Changed site title, tagline, and default language to pt-BR for the ELT82E course. Expanded footer links with UTFPR, developer, tools, and social resources. Added dynamic copyright string with UTC-3 timestamp.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,13 +1,27 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
+const formatter = new Intl.DateTimeFormat("pt-BR", {
+  timeZone: "America/Sao_Paulo",
+  year: "numeric",
+  month: "2-digit",
+  day: "2-digit",
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
+  hourCycle: "h23", // Use 24-hour format
+});
+const utc3Time = formatter.format(new Date());
+const COPYRIGHT_STRING = `Copyright © ${new Date().getFullYear()} sd-docs, Inc. Built with Docusaurus at ${utc3Time} UTC-3.`;
+
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+  title: "ELT82E - Eletrônica Analógica",
+  tagline:
+    "Polarizar componentes ativos para atuar na região de corte, saturação e linear. Ensaiar circuitos eletrônicos contendo componentes ativos e passivos.",
+  favicon: "img/favicon.ico",
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
@@ -15,57 +29,55 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://ruseleredu.github.io',
+  url: "https://ruseleredu.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/ea-docs/',
+  baseUrl: "/ea-docs/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'ruseleredu', // Usually your GitHub org/user name.
-  projectName: 'ea-docs', // Usually your repo name.
+  organizationName: "ruseleredu", // Usually your GitHub org/user name.
+  projectName: "ea-docs", // Usually your repo name.
 
   deploymentBranch: "gh-pages", // The branch that GitHub pages will deploy from.
   trailingSlash: false, // Set to true if you want to add a trailing slash to all URLs.
-  onDuplicateRoutes: 'warn',
-  onBrokenLinks: 'throw',
+  onDuplicateRoutes: "warn",
+  onBrokenLinks: "throw",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "pt-BR",
+    locales: ["pt-BR"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
-          sidebarPath: './sidebars.ts',
+          sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/ruseleredu/ea-docs/edit/main/',
+          editUrl: "https://github.com/ruseleredu/ea-docs/edit/main/",
         },
         blog: {
           showReadingTime: true,
           feedOptions: {
-            type: ['rss', 'atom'],
+            type: ["rss", "atom"],
             xslt: true,
           },
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/ruseleredu/ea-docs/edit/main/',
+          editUrl: "https://github.com/ruseleredu/ea-docs/edit/main/",
           // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
+          onInlineTags: "warn",
+          onInlineAuthors: "warn",
+          onUntruncatedBlogPosts: "warn",
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
@@ -73,75 +85,197 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: "img/docusaurus-social-card.jpg",
     colorMode: {
       respectPrefersColorScheme: true,
     },
     navbar: {
-      title: 'My Site',
+      title: "ELT82E",
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        alt: "ELT82E Logo",
+        src: "img/logo.svg",
       },
       items: [
         {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Tutorial',
+          type: "docSidebar",
+          sidebarId: "tutorialSidebar",
+          position: "left",
+          label: "Tutorial",
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
+        { to: "/blog", label: "Blog", position: "left" },
         {
-          href: 'https://github.com/ruseleredu/ea-docs',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/ruseleredu/ea-docs",
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Docs',
+          title: "UTFPR",
           items: [
             {
-              label: 'Tutorial',
-              to: '/docs/intro',
+              label: "Moodle",
+              href: "https://moodle.utfpr.edu.br/",
+            },
+            {
+              label: "Webmail",
+              href: "https://webmail.utfpr.edu.br/",
+            },
+            {
+              label: "Sistemas",
+              href: "https://sistemas2.utfpr.edu.br/",
+            },
+            {
+              label: "SEI",
+              href: "https://sei.utfpr.edu.br/",
+            },
+            {
+              label: "Chat",
+              href: "https://chat.utfpr.edu.br/",
+            },
+            {
+              label: "Ajuda",
+              href: "https://ajuda.utfpr.edu.br/",
+            },
+            {
+              label: "Calendário",
+              href: "https://www.utfpr.edu.br/alunos/calendario",
+            },
+            {
+              label: "TCC",
+              href: "https://nupet.daelt.ct.utfpr.edu.br/tcc/engenharia/index.html",
             },
           ],
         },
         {
-          title: 'Community',
+          title: "Sistemas",
           items: [
             {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              label: "Sigepe",
+              href: "https://sougov.sigepe.gov.br/sougov/Home",
             },
             {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
+              label: "Consulta Diários",
+              href: "https://sistemas2.utfpr.edu.br/url/1141",
             },
             {
-              label: 'X',
-              href: 'https://x.com/docusaurus',
+              label: "Permanências",
+              href: "https://sistemas2.utfpr.edu.br/url/1026",
+            },
+            {
+              label: "Planejamento",
+              href: "https://sistemas2.utfpr.edu.br/url/1890",
+            },
+            {
+              label: "Lançamentos",
+              href: "https://sistemas2.utfpr.edu.br/url/1514",
+            },
+            {
+              label: "Mapa de Professor",
+              href: "https://sistemas2.utfpr.edu.br/url/1022",
+            },
+            {
+              label: "Mapa de Sala",
+              href: "https://sistemas2.utfpr.edu.br/url/1934",
             },
           ],
         },
         {
-          title: 'More',
+          title: "Desenvolvedor",
           items: [
             {
-              label: 'Blog',
-              to: '/blog',
+              label: "Visual Studio Code",
+              href: "https://code.visualstudio.com/download",
             },
             {
-              label: 'GitHub',
-              href: 'https://github.com/ruseleredu/ea-docs',
+              label: "Git SCM",
+              href: "https://git-scm.com/downloads",
+            },
+            {
+              label: "GitHub CLI",
+              href: "https://cli.github.com/",
+            },
+            {
+              label: "GitHub Desktop",
+              href: "https://desktop.github.com/download/",
+            },
+            {
+              label: "PlatformIO",
+              href: "https://platformio.org//install/ide?install=vscode",
+            },
+            {
+              label: "Arduino IDE",
+              href: "https://www.arduino.cc/en/software/",
+            },
+          ],
+        },
+        {
+          title: "Ferramentas",
+          items: [
+            {
+              label: "Karnaugh Map Solver",
+              href: "https://wredan.github.io/Karnaugh-Map-Solver/",
+            },
+            {
+              label: "Tinkercad Circuits",
+              href: "https://www.tinkercad.com/circuits",
+            },
+            {
+              label: "Logic circuit simplification",
+              href: "http://www.32x8.com/index.html",
+            },
+            {
+              label: "Quine McCluskey Solver",
+              href: "https://quinemccluskey.geeekyboy.com/",
+            },
+            {
+              label: "PlatformIO",
+              href: "https://platformio.org//install/ide?install=vscode",
+            },
+            {
+              label: "WinGet",
+              href: "https://winget.run/",
+            },
+          ],
+        },
+        {
+          title: "Social",
+          items: [
+            {
+              label: "YouTube",
+              href: "https://www.youtube.com/@Rusla82",
+            },
+            {
+              label: "YouTube Edu",
+              href: "https://www.youtube.com/@adrianoruseler653",
+            },
+            {
+              label: "GitHub",
+              href: "https://github.com/AdrianoRuseler/",
+            },
+            {
+              label: "GitHub Edu",
+              href: "https://github.com/ruseleredu/",
+            },
+            {
+              label: "LinkedIn",
+              href: "https://www.linkedin.com/in/adrianoruseler/",
+            },
+            {
+              label: "ResearchGate",
+              href: "https://www.researchgate.net/profile/Adriano-Ruseler/",
+            },
+            {
+              label: "ORCID",
+              href: "https://orcid.org/0000-0003-0915-9483",
             },
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: COPYRIGHT_STRING,
     },
     prism: {
       theme: prismThemes.github,
