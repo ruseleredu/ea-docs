94cbcb36d5a2e3e99720e9aecc3c5f0cb1af06ec
Adriano Ruseler <adrianoruseler@gmail.com>
2026-01-26 15:21:47 -0300

    Update Prism (Code Block)

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -314,6 +314,18 @@ const config: Config = {
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
+      additionalLanguages: [
+        "autohotkey",
+        "arduino",
+        "apacheconf",
+        "nginx",
+        "bash",
+        "matlab",
+        "powershell",
+        "ini",
+        "latex",
+        "java",
+      ],
     },
   } satisfies Preset.ThemeConfig,
 };
